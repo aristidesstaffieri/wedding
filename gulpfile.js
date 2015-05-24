c986575336288a1196427867f4dedd86f8f69064
Aristides Staffieri <aristides@aristidesstaffieri.com>
2015-05-24 16:28:58 -0600

    Minifying css, concat and uglify JS, linted code, removed extar snippets

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ var sass = require('gulp-ruby-sass');
 var plumber = require('gulp-plumber');
 var livereload = require('gulp-livereload');
 var server = require('gulp-develop-server');
+var concat = require('gulp-concat');
+var minifycss = require('gulp-minify-css');
 
 var options = {
     path: 'app.js'
@@ -21,9 +23,18 @@ gulp.task( 'server:start', function() {
 
 // Run before deployment and publish build folder
 gulp.task('build', function() {
-	gulp.src('public/js/*.js')
+	gulp.src('public/js/**/*.js')
+	.pipe(concat('app.js'))
 	.pipe(uglify())
 	.pipe(gulp.dest('build/js'));
+});
+
+gulp.task('minify', function() {
+	sass('public/stylesheets/', {})
+	.pipe(plumber())
+	.pipe(gulp.dest('build/stylesheets'))
+	.pipe(minifycss())
+	.pipe(gulp.dest('build/stylesheets'));
 });
 
 gulp.task('styles', function() {
